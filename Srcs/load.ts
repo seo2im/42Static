3396995eda46e036a8915fb2b2e9801fa7775d37
seo2im <seo2im6492@gmail.com>
2021-04-12 13:14:58 +0900

    Browser testing
    Done
    1. DB connecting & save data in local db
    2. Testing in browser parcel
    
    TODO
    1. Not sure, but mongoose cant use browser site....
    2. Make Express Admin site

--- Srcs/load.ts
+++ Srcs/load.ts
@@ -1,8 +1,151 @@
 import fs from 'fs'
 import path from 'path'
 import { tUser } from '../Types'
+import mongoose, { model, Schema } from 'mongoose'
+import { DB_USER, DB_PASSWORD } from './Secret'
+/*
+    load fron DB
+*/
+const URL = `mongodb://${DB_USER}:${DB_PASSWORD}@localhost:27017/admin`
+const dbConnect = async () => {
+    await mongoose.connect(URL, {
+        dbName: "42Static",
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => {
+        console.log('DB Connected!')
+    }).catch(err => {
+        console.log(err)
+    })
+}
+const User = new Schema({
+    "id": {
+      "type": "Number"
+    },
+    "email": {
+      "type": "String"
+    },
+    "login": {
+      "type": "String"
+    },
+    "first_name": {
+      "type": "String"
+    },
+    "last_name": {
+      "type": "String"
+    },
+    "usual_first_name": {
+      "type": "Mixed"
+    },
+    "url": {
+      "type": "String"
+    },
+    "phone": {
+      "type": "String"
+    },
+    "displayname": {
+      "type": "String"
+    },
+    "usual_full_name": {
+      "type": "String"
+    },
+    "image_url": {
+      "type": "String"
+    },
+    "staff?": {
+      "type": "Boolean"
+    },
+    "correction_point": {
+      "type": "Number"
+    },
+    "pool_month": {
+      "type": "String"
+    },
+    "pool_year": {
+      "type": "Date"
+    },
+    "location": {
+      "type": "Mixed"
+    },
+    "wallet": {
+      "type": "Number"
+    },
+    "anonymize_date": {
+      "type": "Date"
+    },
+    "groups": {
+      "type": "Array"
+    },
+    "cursus_users": {
+      "type": [
+        "Mixed"
+      ]
+    },
+    "projects_users": {
+      "type": [
+        "Mixed"
+      ]
+    },
+    "languages_users": {
+      "type": [
+        "Mixed"
+      ]
+    },
+    "achievements": {
+      "type": [
+        "Mixed"
+      ]
+    },
+    "titles": {
+      "type": "Array"
+    },
+    "titles_users": {
+      "type": "Array"
+    },
+    "partnerships": {
+      "type": "Array"
+    },
+    "patroned": {
+      "type": "Array"
+    },
+    "patroning": {
+      "type": "Array"
+    },
+    "expertises_users": {
+      "type": "Array"
+    },
+    "roles": {
+      "type": "Array"
+    },
+    "campus": {
+      "type": [
+        "Mixed"
+      ]
+    },
+    "campus_users": {
+      "type": [
+        "Mixed"
+      ]
+    }
+  })
+const userModel = model('users', User, 'users')
 
-const loadUsers: () => tUser[] = () => {
+const loadDataUsersInDB = async () => {
+    await dbConnect()
+
+    try {
+        const users = await userModel.find()
+        return users
+    } catch (err) {
+        console.log(err)
+    }
+
+}
+
+/*
+    load client terminal
+*/
+const loadUsersInTerminal: () => tUser[] = () => {
     const files :string[] = fs.readdirSync(path.join(__dirname, '../Data/users'))
 
     let fileString: string
@@ -15,4 +158,4 @@ const loadUsers: () => tUser[] = () => {
     return Users
 }
 
-export { loadUsers }
+export { loadUsersInTerminal, loadDataUsersInDB }
--- index.ts
+++ index.ts
@@ -1,3 +1,34 @@
-import { loadUsers } from './Srcs/load'
+import { loadUsersInTerminal, loadDataUsersInDB } from './Srcs/load'
 
-console.log(loadUsers());
+/*
+    In Client Terminal
+*/
+/*
+const users = loadUsersInTerminal()
+const cursus = users.filter(user => user.cursus_users.length > 1)
+
+console.log(cursus.length)
+
+const member = cursus.filter(user => user.cursus_users[1].grade === "Member")
+console.log(member.map(user => user.login))
+*/
+
+/*
+    In DB Connect
+*/
+const main = async () => {
+    const users = await loadDataUsersInDB()
+    if (users)
+        console.log(users.length)
+    else
+        console.log("error in load")
+    /*
+    const h3 = document.createElement('h3')
+    const text = document.createTextNode(`${users[0].login}`)
+    h3.appendChild(text);
+    document.body.appendChild(h3)
+    */
+    
+}
+
+main()
