ab6bcf8345ef8cfdd754e8fcda54ea55ede6c483
seo2im <seo2im6492@gmail.com>
2021-04-17 18:43:22 +0900

    Login setting
    DONE
    1. Login passport oauth setting
    
    TODO
    1. token authentication after login page

--- index.ts
+++ index.ts
@@ -7,66 +7,82 @@ import Connect from './Srcs/Model/connect'
 import { LoadUsers } from './Srcs/load'
 import { getInProgressCount } from './Srcs/parse'
 
+import bodyParser from 'koa-bodyparser'
+import session from 'koa-session'
+
 import passport from 'koa-passport'
 import { Strategy } from 'passport-42'
-import { ensureLoggedIn } from 'connect-ensure-login'
-
+/*
+*    DB CONNECT & INITIALIZE APP
+*/
 const app = new Koa()
 const router = new Router()
 Connect()
-
+/*
+*   SESSION SETTING
+*/
+app.keys = ['key'] /* Have to know what this setting need */
+app.use(bodyParser())
+const sessionConfig = {
+    
+}
+app.use(session(sessionConfig, app))
+/*
+*   PASSPORT SETTING
+*/
 passport.use(new Strategy({
     clientID: process.env.FT_UUID,
     clientSecret: process.env.FT_SECRET,
     callbackURL: process.env.RETURN_URL,
-}, (accessToken, refreshToken, profile, cb) => {
-    console.log(profile)
-    return cb(null, profile)
+}, (accessToken, refreshToken, profile, done) => {
+    return done(null, profile)
 }))
-
-passport.serializeUser((user, cb) => {
-    cb(null, user)
+passport.serializeUser((user, done) => {
+    done(null, user)
 })
-passport.deserializeUser((user: Express.User, cb) => {
-    cb(null, user)
+passport.deserializeUser((user: Express.User, done) => {
+    console.log(user)
+    done(null, user)
 })
-
-router.use(passport.initialize())
-router.use(passport.session())
-
-app.use(render)
+app.use(passport.initialize())
+app.use(passport.session()) 
+const isLoggedIn = async (ctx: Context, next) => {
+    console.log(ctx.isAuthenticated())
+    //console.log(ctx.state)
+    await next()
+}
 /*
-    router function
+    ROUTING & RENDERRING
 */
+app.use(render)
 const index = async (ctx) => {
     await ctx.render('index.ejs')
 }
-
 const main = async (ctx) => {
     const users = await LoadUsers();
     const ft_server = getInProgressCount(users, 'ft_server')
     const cub3d = getInProgressCount(users, 'cub3d')
     const miniRT = getInProgressCount(users, 'miniRT')
-    console.log(ft_server.length, cub3d.length, miniRT.length)
     if (!users)
         await ctx.render('404.ejs')
     else 
         await ctx.render('main.ejs' , { users: ft_server })
 }
-
 router.get('/', index)
-
 router.get('/login/42', passport.authenticate('42'))
 router.get('/login/42/return',
-    passport.authenticate('42', { failureRedirect: '/' }),
-    (ctx) => {
-        return ctx.redirect('/main')
-    }
+    passport.authenticate('42', { 
+        successRedirect: '/main',
+        failureRedirect: '/'
+    }),
+    
 )
-
-router.get('/main', main)
-
+app.use(isLoggedIn)
+router.get('/main', isLoggedIn ,main)
 app.use(router.routes())
+/*
+    OPEN SERVER
+*/
 app.listen(4000, () => {
     console.log(`server ready port: 4000`)
 })
