ab8157139b5412eadbd4ca8c8c35c5ef57df856f
seo2im <seo2im6492@gmail.com>
2021-04-29 23:21:04 +0900

    refector: FrontEnd react 환경 셋팅 Do 1. React 환경 업데이트 	- typescript + webpack 환경 구축 	- awesome-typescript-loader사용
    resolve: #1

---
+++ client/config/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { TsConfigPathsPlugin } = require('awesome-typescript-loader')
+
+module.exports = {
+    entry: path.join(__dirname, '../srcs/index.tsx'),
+    target: 'web',
+    mode: 'development',
+
+    output: {
+        filename: '[name].bundle.js',
+        path: path.join(__dirname, '../build'),
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader',
+            },
+        ]
+    },
+    devServer : {
+		historyApiFallback : true,
+		contentBase : path.join(__dirname, '../build'),
+		port : 4000,
+	},
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../public', 'index.html')
+        }),
+        new TsConfigPathsPlugin()
+    ],
+}
